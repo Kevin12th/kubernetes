7919a9b6891a88a24b61a02e389ccb4f6ac87411
wang.lei <wang.lei@astute-tec.com>
2023-08-30 16:35:55 +0800

    first commit

---
+++ staging/src/k8s.io/component-base/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "24"
+	gitVersion   = "v1.24.9-k3s1"
+	gitCommit    = "54026d00db09a279e302b1ed468c1ad2ca6d9ce2"
+	gitTreeState = "clean"
+	buildDate = "2022-12-08T23:34:11Z"
+)
